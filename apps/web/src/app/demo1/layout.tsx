d90ebc1d97f45ff53e05769f49bf93c7de30f906
Dean Kerr <dean.kerr@gmail.com>
2025-11-04 08:39:20 +1100

    cleanup

--- apps/web/src/app/demo1/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next"
-
-export const metadata: Metadata = {
-  title: "demo1",
-}
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return children
-}
